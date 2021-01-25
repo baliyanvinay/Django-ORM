f78af4cfa3443f9538eaa03d9157f1c8b8af0759
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-01-25 21:21:57 +0530

    Teacher & Student Model added

--- college/models.py
+++ college/models.py
@@ -2,5 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name=models.CharField(max_length=30)
-    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    classroom = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
+
+class Teacher(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name
