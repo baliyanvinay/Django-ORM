20df0bb2c9647e7652546db40aaeae1f6a64790a
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-01-31 21:00:54 +0530

    View updated

--- college/views.py
+++ college/views.py
@@ -9,9 +9,10 @@ from college.querysetAPI import (
 def index(request):
     context_01 = method_returning_new_queryset(request)
     context_02 = method_not_returning_new_queryset(request)
-    # queryset_field_lookup(request)
+    context_03 = queryset_field_lookup(request)
 
     return render(request, 'college/index.html', context={
         'context_01': context_01,
-        'context_02': context_02
+        'context_02': context_02,
+        'context_03': context_03,
         })
