09adcd42a52014fd1fa74a966f4c00755a6949a4
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-02-02 09:22:44 +0530

    New function added for q objects

--- college/views.py
+++ college/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 from college.querysetAPI import (
     method_returning_new_queryset,
     method_not_returning_new_queryset,
-    queryset_field_lookup
+    queryset_field_lookup,
+    complex_queries_q_objects
 )
 
 def index(request):
     context_01 = method_returning_new_queryset(request)
     context_02 = method_not_returning_new_queryset(request)
     context_03 = queryset_field_lookup(request)
+    context_04 = complex_queries_q_objects(request)
 
     return render(request, 'college/index.html', context={
         'context_01': context_01,
         'context_02': context_02,
         'context_03': context_03,
+        'context_04': context_04,
         })
